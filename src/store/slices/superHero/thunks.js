9daa21ca6a7da376b6099a65e0df2bd83b825862
hernando-code <hernaranjo@gmail.com>
2023-04-24 16:15:39 -0500

    docs

--- src/store/slices/superHero/thunks.js
+++ src/store/slices/superHero/thunks.js
@@ -13,13 +13,19 @@ export const getSuperHero = (page, indice = 1) => {
         indice = 1;
       }
     }
+
     const superHero = [];
+
+    const fetchSuperHero = async(id) => {
+        const resp = await fetch(
+            `https://www.superheroapi.com/api.php/112776435127359/${id}`
+          );
+          const data = await resp.json();
+          superHero.push(data); 
+    }
+
     for (let i = indice; i <= indice + 10; i++) {
-      const resp = await fetch(
-        `https://www.superheroapi.com/api.php/112776435127359/${i}`
-      );
-      const data = await resp.json();
-      superHero.push(data);
+      await fetchSuperHero(i)
     }
     
     dispatch(setSuperHero({ superHero, page, indice }));
